bd928ebdcd4702f784fe8a1cf691492dde2463a6
Benjamin Marius Lank <benj007d@elevcampus.dk>
2023-10-03 11:21:19 +0200

    Added errorhandling to EventChips.

--- Client/wwwroot/js/EventChips.js
+++ Client/wwwroot/js/EventChips.js
@@ -1,4 +1,4 @@
-﻿export function temp() {
+﻿export function SendHelpToFixClass() {
     var temp = $(".blazored-toast-component")
     temp.removeClass("blazored-toast-component");
     temp.addClass("blazored-toast-info");
